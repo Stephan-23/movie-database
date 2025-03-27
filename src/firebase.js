1ee96150d48ea4793481d97e84cc40c7e53f2e73
Thembinkosi Dladla <thembinkosidladla318@gmail.com>
2025-03-27 15:09:34 +0200

    debug

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,4 @@
-/*import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';const firebaseConfig = {
-    apiKey: "AIzaSyCmQiCA1UHw5ayVOXJMhbdHBGWtxru1-Yo",
-    authDomain: "moviedatabase-beab8.firebaseapp.com",
-    projectId: "moviedatabase-beab8",
-    storageBucket: "moviedatabase-beab8.firebasestorage.app",
-    messagingSenderId: "751874263992",
-    appId: "1:751874263992:web:6a42df07d57a1e7603dd4d",
-    measurementId: "G-59544VNCRX"
-  };// Initialize Firebase
-const app = initializeApp(firebaseConfig);// Initialize Firebase Authentication and Firestore
-export const auth = getAuth(app);
-export const db = getFirestore(app);// Google Auth Provider for sign-in
-export const googleProvider = new GoogleAuthProvider();// Functions for sign-in and sign-out
-export const signInWithGoogle = () => signInWithPopup(auth, googleProvider);
-export const logOut = () => signOut(auth);*/
+
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import { getFirestore, doc, setDoc, getDoc, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore';
